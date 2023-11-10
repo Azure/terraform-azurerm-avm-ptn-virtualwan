00a1dc03159287649e4bc20a693a2acf8870bf69
Jared Holgate <jaredholgate@microsoft.com>
2023-11-10 11:49:52 +0000

    Tidy

--- main.tf
+++ main.tf
@@ -5,8 +5,7 @@ data "azurerm_resource_group" "rg" {
 
 # Create rgs as defined by var.virtual_wan
 resource "azurerm_resource_group" "rg" {
-  count = var.create_resource_group ? 1 : 0
-
+  count    = var.create_resource_group ? 1 : 0
   location = var.location
   name     = var.resource_group_name
   tags     = try(merge(var.resource_group_tags, var.tags), {})

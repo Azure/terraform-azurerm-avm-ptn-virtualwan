b9c81f36f996840f53a743f9bbe886c4d384d6a7
khushal <khushal08@yahoo.com>
2024-01-29 17:21:59 +1100

    Releasing newer minor version (#21)
    * Releasing newer minor version
    
    * Corrected module name
    
    * Corrected module type

--- locals.telemetry.tf
+++ locals.telemetry.tf
@@ -1,6 +1,21 @@
 locals {
-  module_name = "authorization-roleassignment"
-  module_type = "res"
+
+  # This is the unique id AVM Terraform modules that is supplied by the AVM team.
+  # See https://azure.github.io/Azure-Verified-Modules/specs/shared/#id-sfr3---category-telemetry---deploymentusage-telemetry
+  telem_puid = "46d3xgtf"
+
+  # TODO: change this to the name of the module. See https://azure.github.io/Azure-Verified-Modules/specs/shared/#id-sfr3---category-telemetry---deploymentusage-telemetry
+  module_name = "virtualwan"
+
+  # TODO: Change this. Should be either `res` or `ptn`
+  module_type = "ptn"
+
+  # This ensures we don't get errors if telemetry is disabled.
+  telem_random_hex = can(random_id.telemetry[0].hex) ? random_id.telemetry[0].hex : ""
+
+  # This constructs the ARM deployment name that is used for the telemetry.
+  # We shouldn't ever hit the 64 character limit but use substr just in case.
+
   telem_arm_deployment_name = substr(
     format(
       "%s.%s.%s.v%s.%s",
@@ -13,6 +28,10 @@ locals {
     0,
     64
   )
+
+
+  # This is an empty ARM deployment template.
+
   telem_arm_template_content = <<TEMPLATE
 {
   "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
@@ -23,11 +42,16 @@ locals {
   "outputs": {
     "telemetry": {
       "type": "String",
+
+
       "value": "For more information, see https://aka.ms/avm/telemetry"
+
     }
   }
 }
 TEMPLATE
+
   telem_puid                 = "46d3xgtf"
   telem_random_hex           = can(random_id.telem[0].hex) ? random_id.telem[0].hex : ""
 }
+

b9ab6aac1be2fedd6febd4cfa778ac861a26048f
ChrisSidebotham-MSFT <48600046+ChrisSidebotham@users.noreply.github.com>
2024-05-24 14:37:15 +0100

    chore: Update VPN gateway SKU and virtual network gateway SKU to AzVpnGw1

--- examples/s2svpn/customer_vpn.tf
+++ examples/s2svpn/customer_vpn.tf
@@ -42,6 +42,7 @@ resource "azurerm_public_ip" "gw_ip" {
   location            = azurerm_resource_group.rg.location
   name                = local.on_prem_public_ip_name
   resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Standard"
 }
 
 # Create virtual network gateway

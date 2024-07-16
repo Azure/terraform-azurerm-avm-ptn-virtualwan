a71b1eb47f503f83678bb1861480f86cdb52deb6
Jon Chancellor <jchancellor@microsoft.com>
2024-07-16 15:47:57 -0700

    Telemetry,output, and issue fixes

--- outputs.tf
+++ outputs.tf
@@ -47,3 +47,53 @@ output "virtual_wan_id" {
   description = "Virtual WAN ID"
   value       = azurerm_virtual_wan.virtual_wan != null ? azurerm_virtual_wan.virtual_wan.id : null
 }
+
+output "azure_firewall_resource_ids" {
+  description = "A map of Azure Firewall resource IDs with the map keys of the `firewalls` variable."
+  value       = var.firewalls != null ? { for key, value in azurerm_firewall.fw : key => value.id } : null
+}
+
+output "expressroute_gateway_resource_ids" {
+  description = "A map of expressRoute Gateway IDs with the map keys of the `expressroute_gateways` variable."
+  value       = var.expressroute_gateways != null ? { for key, value in azurerm_express_route_gateway.express_route_gateway : key => value.id } : null
+}
+
+output "p2s_vpn_gw_resource_ids" {
+  description = "A map of point to site VPN gateway names with the map keys of the `p2s_gateways` variable."
+  value       = var.p2s_gateways != null ? { for key, value in azurerm_vpn_gateway.vpn_gateway : key => value.id } : null
+}
+
+output "vpn_gateway_resource_ids" {
+  description = "A map of Azure VPN Gateway resource IDs with the map keys of the `vpn_gateways` variable."
+  value       = var.vpn_gateways != null ? { for key, value in azurerm_vpn_gateway.vpn_gateway : key => value.id } : null
+}
+
+output "virtual_hub_resource_ids" {
+  description = "A map of Azure Virtual Hub resource IDs with the map keys of the `virtual_hubs` variable."
+  value       = var.virtual_hubs != null ? { for key, value in azurerm_virtual_hub.virtual_hub : key => value.id } : null
+}
+
+output "azure_firewall_resource_names" {
+  description = "A map of Azure Firewall resource names with the map keys of the `firewalls` variable."
+  value       = var.firewalls != null ? { for key, value in azurerm_firewall.fw : key => value.id } : null
+}
+
+output "expressroute_gateway_resource_names" {
+  description = "A map of expressRoute Gateway names with the map keys of the `expressroute_gateways` variable."
+  value       = var.expressroute_gateways != null ? { for key, value in azurerm_express_route_gateway.express_route_gateway : key => value.id } : null
+}
+
+output "p2s_vpn_gw_resource_names" {
+  description = "A map of point to site VPN gateway names with the map keys of the `p2s_gateways` variable."
+  value       = var.p2s_gateways != null ? { for key, value in azurerm_vpn_gateway.vpn_gateway : key => value.id } : null
+}
+
+output "vpn_gateway_resource_names" {
+  description = "A map of Azure VPN Gateway resource names with the map keys of the `vpn_gateways` variable."
+  value       = var.vpn_gateways != null ? { for key, value in azurerm_vpn_gateway.vpn_gateway : key => value.id } : null
+}
+
+output "virtual_hub_resource_names" {
+  description = "A map of Azure Virtual Hub resource names with the map keys of the `virtual_hubs` variable."
+  value       = var.virtual_hubs != null ? { for key, value in azurerm_virtual_hub.virtual_hub : key => value.id } : null
+}

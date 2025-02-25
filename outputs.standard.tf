ec87b8684ab1bc147bdc5724e6449fbed974a222
Jared Holgate <jaredholgate@microsoft.com>
2025-02-25 12:16:49 +0000

    Add firewall outputs mapped by hub key

--- outputs.standard.tf
+++ outputs.standard.tf
@@ -28,6 +28,26 @@ output "firewall_public_ip_addresses" {
   value       = module.firewalls.public_ip_addresses
 }
 
+output "firewall_resource_ids_by_hub_key" {
+  description = "A map of Azure Firewall resource IDs with the map keys of the `firewalls` variable."
+  value       = { for key, value in var.firewalls : value.virtual_hub_key => module.firewalls.resource_ids[key] }
+}
+
+output "firewall_resource_names_by_hub_key" {
+  description = "A map of Azure Firewall resource names with the map keys of the `firewalls` variable."
+  value       = { for key, value in var.firewalls : value.virtual_hub_key => module.firewalls.resource_names[key] }
+}
+
+output "firewall_private_ip_addresses_by_hub_key" {
+  description = "A map of Azure Firewall private IP addresses with the map keys of the `firewalls` variable."
+  value       = { for key, value in var.firewalls : value.virtual_hub_key => module.firewalls.private_ip_addresses[key] }
+}
+
+output "firewall_public_ip_addresses_by_hub_key" {
+  description = "A map of Azure Firewall public IP addresses with the map keys of the `firewalls` variable."
+  value       = { for key, value in var.firewalls : value.virtual_hub_key => module.firewalls.public_ip_addresses[key] }
+}
+
 output "virtual_hub_resource_ids" {
   description = "A map of Azure Virtual Hub resource IDs with the map keys of the `virtual_hubs` variable."
   value       = module.virtual_hubs.resource_ids

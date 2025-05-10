d9ea4ffc160b675842152e4392b04a89a8dfa5dc
Ketil Gjerde <477141+mysteq@users.noreply.github.com>
2025-05-10 22:55:27 +0200

    feat: support setting sku on virtual_hubs
    Signed-off-by: Ketil Gjerde <477141+mysteq@users.noreply.github.com>

--- modules/virtualhub/variables.tf
+++ modules/virtualhub/variables.tf
@@ -4,6 +4,7 @@ variable "virtual_hubs" {
     location                               = string
     resource_group                         = optional(string, null)
     address_prefix                         = string
+    sku                                    = optional(string, null)
     tags                                   = optional(map(string))
     virtual_wan_id                         = string
     hub_routing_preference                 = optional(string, "ExpressRoute")
@@ -19,6 +20,7 @@ variable "virtual_hubs" {
   - `location`: Location for the Virtual Hub resource.
   - `resource_group`: Optional resource group name to deploy the Virtual Hub into. If not specified, the Virtual Hub will be deployed into the resource group specified in the variable `resource_group_name`, e.g. the same as the Virtual WAN itself.
   - `address_prefix`: Address prefix for the Virtual Hub. Recommend using a `/23` CIDR block.
+  - `sku`: Optional SKU for the Virtual Hub. Possible values are: `Standard` or `Basic`.
   - `tags`: Optional tags to apply to the Virtual Hub resource.
   - `hub_routing_preference`: Optional hub routing preference for the Virtual Hub. Possible values are: `ExpressRoute`, `ASPath`, `VpnGateway`. Defaults to `ExpressRoute`. See https://learn.microsoft.com/azure/virtual-wan/hub-settings#routing-preference for more information.
   - `virtual_router_auto_scale_min_capacity`: Optional minimum capacity for the Virtual Router auto scale. Defaults to `2`. See https://learn.microsoft.com/azure/virtual-wan/hub-settings#capacity for more information.

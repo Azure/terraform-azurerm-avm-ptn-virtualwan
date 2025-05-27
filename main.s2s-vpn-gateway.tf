4641f365f6ebb67f40ad56daff70be2e583ac880
Jared Holgate <jaredholgate@microsoft.com>
2025-05-27 20:07:14 +0100

    feat: improvements and fixes for migration (#172)
    * feat: improvements and fixes for migration
    
    * improve file names and add exception to test

--- main.s2s-vpn-gateway.tf
+++ main.s2s-vpn-gateway.tf
@@ -1,25 +1,3 @@
-module "vpn_gateway" {
-  source = "./modules/site-to-site-gateway"
-
-  vpn_gateways = {
-    for key, value in local.vpn_gateways : key => {
-      name                                  = value.name
-      resource_group_name                   = module.virtual_hubs.resource_object[value.virtual_hub_key].resource_group
-      location                              = module.virtual_hubs.resource_object[value.virtual_hub_key].location
-      virtual_hub_id                        = module.virtual_hubs.resource_object[value.virtual_hub_key].id
-      bgp_route_translation_for_nat_enabled = value.bgp_route_translation_for_nat_enabled
-      scale_unit                            = value.scale_unit
-      routing_preference                    = value.routing_preference
-      bgp_settings                          = value.bgp_settings
-    }
-  }
-}
-
-moved {
-  from = azurerm_vpn_gateway.vpn_gateway
-  to   = module.vpn_gateway.azurerm_vpn_gateway.vpn_gateway
-}
-
 # Create a vpn site. Sites represent the Physical locations (On-Premises) you wish to connect.
 module "vpn_site" {
   source = "./modules/site-to-site-vpn-site"
@@ -84,8 +62,6 @@ module "vpn_site_connection" {
     }
 
   }
-
-  depends_on = [module.vpn_site, module.vpn_gateway]
 }
 
 moved {

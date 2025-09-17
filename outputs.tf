4775dc3e6443311835702b6c794c41c964250c6d
Sherbaz Mohamed <sherbaz@users.noreply.github.com>
2025-09-17 17:12:16 +1000

    Corrected the description for output "ergw_resource_ids_by_hub_key"

--- outputs.tf
+++ outputs.tf
@@ -14,7 +14,7 @@ output "ergw_id" {
 }
 
 output "ergw_resource_ids_by_hub_key" {
-  description = "A map of Azure Firewall resource IDs with the map keys of the `firewalls` variable."
+  description = "A map of ExpressRoute Gateway resource IDs with the map keys of the `expressroute_gateways` variable."
   value       = try(var.expressroute_gateways, null) != null ? { for key, value in var.expressroute_gateways : value.virtual_hub_key => module.express_route_gateways.resource_object[key].id } : null
 }
 
